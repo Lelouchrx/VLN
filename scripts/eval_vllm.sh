976c82f19e031edc8c2307ccb34b160831e41755
LingyiHongfd <honglyhly@gmail.com>
2026-05-17 06:34:05 +0000

    update eval

--- scripts/eval_vllm.sh
+++ scripts/eval_vllm.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=`pwd`:$PYTHONPATH
 
 #R2R
 CONFIG_PATH="config/vln_r2r.yaml"
-SAVE_PATH="/home/cs22-hongly/VLN/results/navida3"
+SAVE_PATH="/home/cs22-hongly/VLN/results/qwen3vl_4b_navida_r2r_vln_90_pass4"
 
 #RxR
 # CONFIG_PATH="config/vln_rxr.yaml"
@@ -13,17 +13,16 @@ SAVE_PATH="/home/cs22-hongly/VLN/results/navida3"
 CHUNKS=16 # which is also the number of simulators to launch during evaluation
 
 export OPENAI_API_KEY="EMPTY"
-export OPENAI_API_BASE="http://10.176.62.172:8001/v1"
+export OPENAI_API_BASE="http://10.176.62.172:8002/v1"
 
-CUDA_VISIBLE_DEVICES=5 python vln/eval_vllm.py \
+CUDA_VISIBLE_DEVICES=6 python vln/eval_vllm_navida.py \
     --exp-config $CONFIG_PATH \
     --split-num $CHUNKS \
     --forward-distance 25 \
     --turn-angle 15 \
     --max-action-history 200 \
     --num-generations 1 \
-    --do-sample False \
-    --use-model-defaults False \
+    --pass-k 4 \
     --result-path $SAVE_PATH
     
 # python src/eval/analyze_results.py \
